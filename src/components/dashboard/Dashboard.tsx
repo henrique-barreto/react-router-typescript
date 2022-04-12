f218004173a0ace151a07d463076de486e26b78f
henrique-barreto <henriquegbar@gmail.com>
2022-04-12 11:26:28 -0300

    3 files modified, 6 added

---
+++ src/components/dashboard/Dashboard.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Dashboard.module.scss';
+
+export default function Dashboard() {
+  return (
+    <div className={styles.background}>
+      <h2>Dashboard dfg</h2>
+    </div>
+  );
+}
